277e951aa36d6fcbe77843d2649fcc9f45954267
(Jonathan) Luke Green <me@lukegreen.xyz>
2025-05-21 19:07:02 -0500

    CRUD (well, CUD) for plans (#5)
    This adds create, update, and delete functionality for budget plans, as
    well as adds a heavily leveraged `window.toggleVisibility` function that
    allows for lots of the enabled show/hide functionality

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,16 @@
 import './bootstrap';
+
+window.toggleVisibility = function (selector) {
+    const el = document.querySelector(selector);
+    const current = el.style.display;
+
+    if (current == "none") {
+        // If we toggled to not visible, the previous value should be stored
+        // If we didn't then set it to ""
+        el.style.display = el['data-toggle-visibility-from'] || "";
+        el['data-toggle-visibility-from'] = undefined;
+    } else {
+        el['data-toggle-visibility-from'] = current;
+        el.style.display = 'none';
+    }
+}
